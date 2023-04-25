4cf0103c9d2fca4fae8de305077e17b671204117
andrejarl <104331025+AndreJarl@users.noreply.github.com>
2023-04-25 16:42:33 +0800

    Update about.js

--- about.js
+++ about.js
@@ -11,6 +11,36 @@ const back = document.getElementById('back');
 const burger = document.getElementById('hu');
 const ex = document.getElementById('huu');
 
+function updateAge() {
+	// Get the current date and the date of your birth
+	const now = new Date();
+	const birthday = new Date('2004-08-12'); // replace with your birth date
+	
+	// If your birthday has not yet passed this year, subtract 1 from the year
+	if (now < new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate())) {
+	  birthday.setFullYear(birthday.getFullYear() - 1);
+	}
+	
+	// Calculate your age in years
+	let age = now.getFullYear() - birthday.getFullYear();
+	
+	// If your birthday has not yet passed this year, subtract 1 from your age
+	if (now < new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate())) {
+	  age--;
+	}
+	
+	// Update the text in the HTML element
+	const element = document.getElementById('age');
+	element.textContent = `${age} years old`;
+  }
+  
+  // Call the updateAge() function to update the age immediately
+  updateAge();
+  
+  // Set the interval to update the age every minute
+  setInterval(updateAge, 60000); // 60000 milliseconds = 1 minute
+  
+
 dark.addEventListener("click", () => {
 			nave.classList.toggle("active");
 	   andre.classList.toggle("active");
